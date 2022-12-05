8eefd0f79bc731d81c14cf676eeb710a8bd5a92d
zhuckov <zhuckovkj@gmail.com>
2022-12-06 00:55:45 +0300

    z

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const path = require('path'); 
 
 const app = express(); 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 app.get('/', (req , res) =>{
-    res.render('pages/fakebus')
-})
-app.get('/setting', (req,res) =>{ 
-    res.render('pages/ticket-setting')
-})
-app.listen(3000, '0.0.0.0')
+    res.render('pages/main.ejs' , {
+        'page': 'fakebus', 
+        'title': 'Главная'
+    });
+});
+app.get('/setting', (req , res) =>{
+    res.render('pages/main.ejs' , {
+        'page': 'ticket-setting', 
+        'title': 'Настройки'
+    });
+});
+app.get('/profile', (req , res) =>{
+    res.render('pages/main.ejs' , {
+        'page': 'profile', 
+        'title': 'Профиль'
+    });
+});
+
+app.listen(3000, '0.0.0.0');
